5b56c53837de46eac1a6cc6cd955645debd9f5d0
Matheus Correia <matheus.correia@finderup.com.br>
2025-02-24 16:11:57 -0300

    minor: enable validation pipes globally

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,9 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
+
+  app.setGlobalPrefix('/api');
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 }

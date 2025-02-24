3263a342a020bf1b6deeb81b241257caaf0d7c59
Matheus Correia <matheus.correia@finderup.com.br>
2025-02-24 16:16:50 -0300

    feat: add open api example to create product

--- backend/src/application/dto/product/create-product.dto.ts
+++ backend/src/application/dto/product/create-product.dto.ts
@@ -1,19 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({ example: 'Product 1' })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({ example: 'A product description' })
   @IsNotEmpty()
   @IsString()
   description: string;
 
+  @ApiProperty({ example: '10.99' })
   @IsNotEmpty()
   @IsNumber()
   @Min(0)
   price: number;
 
+  @ApiProperty({ example: '#ff0000' })
   @IsNotEmpty()
   @IsString()
   color: string;

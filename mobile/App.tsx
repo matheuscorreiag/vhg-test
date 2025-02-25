55299794b0e46ec8154910d37c7af3f3cb8ceb84
Matheus Correia <matheuscorreiags@gmail.com>
2025-02-25 14:12:35 -0300

    feat: initial mobile commit

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,24 +1,13 @@
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
+import './global.css';
 
-
-
-  import './global.css';
-
-
-
-
-
-  export default function App() {
-    return (
-      <>
-        <ScreenContent title="Home" path="App.tsx">
-        
-        </ScreenContent>
-        <StatusBar style="auto" />
-      </>
-    );
-  }
-
-
+export default function App() {
+  return (
+    <>
+      <ScreenContent title="Home" path="App.tsx" />
+      <StatusBar style="auto" />
+    </>
+  );
+}

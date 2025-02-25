0e4079892a82ca4fa033ade3b23f4a940c44d31d
Matheus Correia <matheuscorreiags@gmail.com>
2025-02-25 02:26:26 -0300

    feat: generate token on signup and login

--- backend/src/user/infra/controllers/create-user.controller.ts
+++ backend/src/user/infra/controllers/create-user.controller.ts
@@ -1,5 +1,6 @@
 import { ResponseHelper } from '@helpers/responses';
 import { Body, Controller, Post } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from '@user/application/dto/create-user.dto';
 import { CreateUserUseCase } from '@user/application/use-cases/create-user.use-case';
 import { Public } from '@user/infra/decorators/public.decorators';
@@ -9,15 +10,22 @@ import { Public } from '@user/infra/decorators/public.decorators';
   version: '1',
 })
 export class CreateUserController {
-  constructor(private readonly createUserUseCase: CreateUserUseCase) {}
+  constructor(
+    private readonly createUserUseCase: CreateUserUseCase,
+    private readonly jwtService: JwtService,
+  ) {}
 
   @Public()
   @Post('/signup')
   async createUser(@Body() createUserDto: CreateUserDto) {
     try {
       const user = await this.createUserUseCase.execute(createUserDto);
+      const payload = { email: user.email, id: user.id };
 
-      return ResponseHelper.success({ ...user, token: '' });
+      return ResponseHelper.success({
+        ...user,
+        token: await this.jwtService.signAsync(payload),
+      });
     } catch (error) {
       return { error: error.message };
     }
--- backend/src/user/infra/controllers/login-user.controller.ts
+++ backend/src/user/infra/controllers/login-user.controller.ts
@@ -1,5 +1,6 @@
 import { ResponseHelper } from '@helpers/responses';
 import { Body, Controller, Post } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { LoginUserDto } from '@user/application/dto/login-user.dto';
 import { FindUserByEmailUseCase } from '@user/application/use-cases/find-user-by-email.use-case';
 import { Public } from '@user/infra/decorators/public.decorators';
@@ -9,15 +10,22 @@ import { Public } from '@user/infra/decorators/public.decorators';
   version: '1',
 })
 export class LoginUserController {
-  constructor(private readonly findUserByEmail: FindUserByEmailUseCase) {}
+  constructor(
+    private readonly findUserByEmail: FindUserByEmailUseCase,
+    private readonly jwtService: JwtService,
+  ) {}
 
   @Public()
   @Post('/login')
   async loginUser(@Body() loginUserDto: LoginUserDto) {
     try {
       const user = await this.findUserByEmail.execute(loginUserDto.email);
+      const payload = { email: user.email, id: user.id };
 
-      return ResponseHelper.success({ ...user, token: '' });
+      return ResponseHelper.success({
+        ...user,
+        token: await this.jwtService.signAsync(payload),
+      });
     } catch (error) {
       return { error: error.message };
     }
